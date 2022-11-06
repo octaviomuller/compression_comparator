833afb32aa436ee2ab21ca22c8654e654a43e65a
octaviomuller <octavio.muller@bleu.com.br>
2022-11-06 20:28:37 -0300

    comentários

--- compressor.py
+++ compressor.py
@@ -4,9 +4,10 @@ import zipfile
 
 from time import time
 
-available_methods = ['zip', 'rar', 'targz']
+available_methods = ['zip', 'rar', 'targz'] # Métodos disponíveis
 
 class Compressor:
+    # Constrututor com método e arquivo alvo
     def __init__(self, method: str, file):
         if not any(extension in method for extension in available_methods):
             raise Exception('Invalid method')
@@ -14,6 +15,7 @@ class Compressor:
         self.method = method
         self.file = file
     
+    # Comprimi arquivo baseado no método
     def compress_file(self):
         if self.method == 'zip':
             result, exec_time = self.zip_compressor()
@@ -22,25 +24,28 @@ class Compressor:
         else:
             result, exec_time = self.targz_compressor()
 
-        return result, exec_time
+        return result, exec_time # Retornar tamanho do arquivo resultante e tempo de execução
 
+    # Compressor zip
     def zip_compressor(self):
-        start = time()
+        start = time() # Horário de execução
 
+        # Instância zip com 'w' de write e método de compressão DEFLATED
         my_zip = zipfile.ZipFile('assets/zip_compression.zip', 'w', compression=zipfile.ZIP_DEFLATED)
         
-        if type(self.file) is str:
-            my_zip.write(self.file)
-        else:
+        if type(self.file) is str: # Caso seja arquivo, comprima
+            my_zip.write(self.file) # Comprimi o arquivo escrevendo no arquivo resultante
+        else: # Caso seja pasta, itere
             for file in self.file:
                 my_zip.write(file)
 
-        my_zip.close()
+        my_zip.close() # Fecha instância
 
-        end = time()
+        end = time() # Horário do término
 
-        return self.get_file_size('assets/zip_compression.zip'), end - start
+        return self.get_file_size('assets/zip_compression.zip'), end - start # Tamanho do arquivo e tempo de execução
 
+    # Compressor rar
     def rar_compressor(self):
         start = time() 
         
@@ -48,14 +53,17 @@ class Compressor:
             file = (self.file,)
         else:
             for file in self.file:
-                file = tuple(self.file)
+                file = tuple(self.file) # Transforma lista em tuple
         
+        # Cria arquivo resultante selecionando o método baseado na string, ou seja, identifica o método pela extensão
+        # Patoolib executa ferramenta externa baseado no nome do aqruivo (exemplo: Se o nome do arquivo possuir '.rar' ele executa o comando rar no terminal)
         patoolib.create_archive('assets/rar_compression.rar', file, verbosity=-1)
 
         end = time()
 
         return self.get_file_size('assets/rar_compression.rar'), end - start
     
+    # Compressor tar.gz
     def targz_compressor(self):
         start = time() 
         
@@ -71,6 +79,8 @@ class Compressor:
 
         return self.get_file_size('assets/targz_compression.tar.gz'), end - start
 
+    
+    # Retorna tamanho do arquivo
     def get_file_size(self, file_name: str):
         file_stats = os.stat(file_name)
         return file_stats.st_size
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from compressor import Compressor, available_methods
 
+# Limpa diretório
 def clear_dir():
   files = os.listdir('assets')
     
@@ -9,40 +10,41 @@ def clear_dir():
 
 
 def main():
-  files = os.listdir('sample')
+  files = os.listdir('sample') # Retorna os arquivos da pasta sample
 
-  files.sort()
+  files.sort() # Ordena a lista de arquivos em ordem alfabética
 
-  # single file
+  # Cenário de compressão de arquivo único (loop de aqruivos)
   for file in files:
-    clear_dir()
+    clear_dir() # Limpa diretório
 
-    sample_size = round(os.stat('sample/%s' % file).st_size / 1000, 3)
+    sample_size = round(os.stat('sample/%s' % file).st_size / 1000, 3) # Retorna tamanho do arquivo original
 
-    print(f'\n{file} = {sample_size} Kb\n')
+    print(f'\n{file} = {sample_size} Kb\n') # Imprimi tamanho do arquivo original
 
+    # Iteração para cada método de compressão
     for method in available_methods:
-      compressor = Compressor(method, 'sample/%s' % file)
-      size, exec_time = compressor.compress_file()
+      compressor = Compressor(method, 'sample/%s' % file) # Cria instância da classe compressora
+      size, exec_time = compressor.compress_file() # Executa compressão do arquivo e retorna tamanho resultante e tempo de execução
 
-      size = round(size / 1000, 3)
-      exec_time = round(exec_time * 1000, 3)
+      size = round(size / 1000, 3) # Transforma byte em kB e arredonda para 3 casas decimais
+      exec_time = round(exec_time * 1000, 3) # Transforma segundos em ms e arredonda 3 casas
 
-      print(f'{method}: {size} Kb, {exec_time} ms, {round((sample_size - size) / exec_time, 3)} Kb/ms')
+      print(f'{method}: {size} Kb, {exec_time} ms, {round((sample_size - size) / exec_time, 3)} Kb/ms') # Imprimi o método, tamanho resultante, tempo de execução e taxa de compressão
   
-  # multiple files
-  clear_dir()
+  # Cenário de compressão de pasta (compactação)
+  clear_dir() # Limpa diretório
 
-  files_paths = [] 
-  dir_sample_size = 0
-  
+  files_paths = [] # Lista de caminhos
+  dir_sample_size = 0 # Tamanho da pasta
+
   for i in range(len(files)):
     files_paths.append('sample/%s' % files[i])
     dir_sample_size = dir_sample_size + os.stat('sample/%s' % files[i]).st_size
 
-  dir_sample_size = round(dir_sample_size / 1000, 3)
+  dir_sample_size = round(dir_sample_size / 1000, 3) # Transforma byte em kB e arredonda para 3 casas decimais
 
-  print(f'\ndirectory size = {dir_sample_size} Kb\n')
+  print(f'\ndirectory size = {dir_sample_size} Kb\n') # Imprimi tamanho da pasta
   
   for method in available_methods:
     compressor = Compressor(method, files_paths)
